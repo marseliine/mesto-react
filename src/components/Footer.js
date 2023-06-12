6060ed9104467623fc0a7e669c0b42d944795f3a
marseliine <78792953+marseliine@users.noreply.github.com>
2023-06-13 00:19:32 +0300

    Delete src/components directory

--- src/components/Footer.js
+++
@@ -1,8 +0,0 @@
-function Footer() {
-    return (
-        <footer className="footer">
-            <p className="footer__copyright">&#169; 2023 Mesto Russia</p>
-        </footer>
-    )
-}
-export default Footer;
